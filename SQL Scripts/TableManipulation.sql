5d843f0e7e09f10c2816d90f76f19236a1af95e4
Jason <jasonleostudios@gmail.com>
2022-12-15 18:12:36 -0500

    Update TableManipulation.sql

--- SQL Scripts/TableManipulation.sql
+++ SQL Scripts/TableManipulation.sql
@@ -1,8 +1,8 @@
 DROP TABLE student;
 
 CREATE TABLE student (
-	student_id INT NOT NULL,
-    student_name VARCHAR(20) NOT NULL,
+	student_id INT,
+    student_name VARCHAR(20),
     major varchar(20),
     PRIMARY KEY (student_id)
 );
@@ -25,3 +25,32 @@ INSERT INTO student VALUES(4, 'Jack', 'Biology');
 INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
 
 SELECT * FROM student;
+
+-- CONSTRAINTS
+-- can also use UNIQUE to signify that each entry in a column has to be unique
+CREATE TABLE student (
+	student_id INT NOT NULL,
+    student_name VARCHAR(20) NOT NULL,
+    major varchar(20) UNIQUE,
+    PRIMARY KEY (student_id)
+);
+
+-- DEFAULT allows a value to be inserted if one is not provided
+CREATE TABLE student (
+	student_id INT,
+    student_name VARCHAR(20),
+    major varchar(20) DEFAULT 'undecided',
+    PRIMARY KEY (student_id)
+);
+
+INSERT INTO student(student_id, student_name) VALUES(1, 'Jack');
+
+-- AUTO_INCREMENT makes it so the value indexs forward everytime a new value is inserted
+CREATE TABLE student (
+	student_id INT AUTO_INCREMENT,
+    student_name VARCHAR(20),
+    major varchar(20),
+    PRIMARY KEY (student_id)
+);
+
+
