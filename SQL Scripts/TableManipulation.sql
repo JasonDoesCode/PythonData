d1841fe090bfe70001691abcbdea587026bf2449
Jason <jasonleostudios@gmail.com>
2022-12-15 17:35:43 -0500

    Update TableManipulation.sql

--- SQL Scripts/TableManipulation.sql
+++ SQL Scripts/TableManipulation.sql
@@ -13,3 +13,15 @@ DESCRIBE student;
 ALTER TABLE student ADD gpa DECIMAL(3, 2);
 
 ALTER TABLE student DROP COLUMN gpa;
+
+-- student_id, student_name, major 
+INSERT INTO student VALUES(1, 'Jack', 'Biology');
+INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+
+-- can specfiy which columns the values should be inserted into
+INSERT INTO student(student_id, student_name) VALUES(3, 'Claire');
+
+INSERT INTO student VALUES(4, 'Jack', 'Biology');
+INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
+
+SELECT * FROM student;
